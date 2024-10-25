0309c1f5d6245675b9c26ceeff336b0384bde81f
delfin <carljefferson.delfin@gmail.com>
2024-10-25 21:44:49 +0800

    secret auth

--- constants.ts
+++ constants.ts
@@ -1,6 +1,7 @@
+import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     Credentials({
       name: "Credentials",
